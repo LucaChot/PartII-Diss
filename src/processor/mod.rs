1256ba87ab678c2d7b7cca8d6de72935267c8dae
lucac-zilliqa <luca.choteborsky@outlook.com>
2023-12-21 23:15:01 +0100

    Implemented fox otto processor communication channels

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, sync::mpsc::{Receiver, Sender, self}};
 use super::BChannel;
 
 /// This function returns a 2D Vec of BChannel<T> representing the broadcast 
@@ -31,6 +31,32 @@ pub fn hashtag_processor<T: Clone + Display>(rows : usize, cols : usize) -> Vec<
     for step in 0..rows {
       processors[step * cols + j].push(std::mem::replace(&mut bchannels[step], BChannel::empty()));
     }
+  }
+  return processors
+}
+
+pub fn fox_otto_processor<T: Clone + Display>(rows : usize, cols : usize) -> Vec<(BChannel<T>, Sender<T>, Receiver<T>)> {
+  let num_processors = cols * rows;
+  let mut processors : Vec<(BChannel<T>, Sender<T>, Receiver<T>)> = Vec::with_capacity(num_processors);
+  for _ in 0..num_processors {
+    let (tx, rx) = mpsc::channel();
+    processors.push((
+      BChannel::empty(),
+      tx,
+      rx));
+  }
+
+  for i in 0..rows {
+    let mut bchannels : Vec<BChannel<T>> = BChannel::new(cols);
+    for step in 0..cols {
+      processors[rows * i + step].0 = std::mem::replace(&mut bchannels[step], BChannel::empty());
+    }
+  }
+  
+  for j in 0..num_processors {
+    let (tx, mut rx) = mpsc::channel();
+    processors[j].1 = tx.clone();
+    processors[( num_processors + j - cols ) % num_processors].2 = std::mem::replace(&mut rx, mpsc::channel().1); 
   }
   return processors
 }
--- src/processor/tests.rs
+++ src/processor/tests.rs
@@ -1,14 +1,16 @@
+use crate::processor::fox_otto_processor;
+
 use super::hashtag_processor;
 
 #[test]
-fn test_correct_length(){
+fn hashtag_correct_length(){
 
   let bchannels = hashtag_processor::<i32>(2, 2);
   assert_eq!(bchannels.len(), 4);
 }
 
 #[test]
-fn test_correct_connection(){
+fn hashtag_correct_connection(){
   let bchannels = hashtag_processor::<i32>(2, 2);
   // Check that horizontal broadcast works
   bchannels[0][0].send(1);
@@ -28,4 +30,42 @@ fn test_correct_connection(){
   assert_eq!(bchannels[1][1].recv().unwrap(), 4);
   assert_eq!(bchannels[3][1].recv().unwrap(), 4);
   
+}
+
+#[test]
+fn fox_otto_correct_length(){
+
+  let bchannels = fox_otto_processor::<i32>(2, 2);
+  assert_eq!(bchannels.len(), 4);
+}
+
+#[test]
+fn fox_otto_correct_connection(){
+  let bchannels = fox_otto_processor::<i32>(2, 2);
+  // Check that horizontal broadcast works
+  let _ = bchannels[0].1.send(0);
+  assert_eq!(bchannels[2].2.recv().unwrap(), 0);
+
+  let _ = bchannels[1].1.send(1);
+  assert_eq!(bchannels[3].2.recv().unwrap(), 1);
+
+  let _ = bchannels[2].1.send(2);
+  assert_eq!(bchannels[0].2.recv().unwrap(), 2);
+
+  let _ = bchannels[3].1.send(3);
+  assert_eq!(bchannels[1].2.recv().unwrap(), 3);
+}
+
+#[test]
+fn fox_otto_correct_broadcast(){
+  let bchannels = fox_otto_processor::<i32>(2, 2);
+
+  // Check that horizontal broadcast works
+  bchannels[0].0.send(0);
+  assert_eq!(bchannels[0].0.recv().unwrap(), 0);
+  assert_eq!(bchannels[1].0.recv().unwrap(), 0);
+
+  bchannels[2].0.send(1);
+  assert_eq!(bchannels[2].0.recv().unwrap(), 1);
+  assert_eq!(bchannels[3].0.recv().unwrap(), 1);
 }
