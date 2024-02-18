059daa9cdbf165a5a4e49b1b4b3fb81d8b98839f
lucac-zilliqa <luca.choteborsky@outlook.com>
2024-02-18 16:17:31 +0000

    Implement individual measurements

---
+++ src/bin/logical_measures/main.rs
@@ -0,0 +1,13 @@
+use Simulator::{Comm, Processor};
+
+fn main() {
+  const MATRIX_WIDTH : usize = 16;
+  const MATRIX_HEIGHT : usize = 16;
+  const PROCESSOR_WIDTH : usize = 3;
+  const PROCESSOR_HEIGHT : usize = 3;
+  let a = vec![vec![1; MATRIX_WIDTH]; MATRIX_HEIGHT];
+  //let b = vec![vec![2; MATRIX_WIDTH]; MATRIX_HEIGHT];
+  let mut p : Processor<isize> = Processor::new(PROCESSOR_HEIGHT,PROCESSOR_WIDTH);
+  let iterations = f64::ceil(f64::log2(a.len() as f64)) as usize;
+  p.parallel_square(a, iterations, Comm::CANNON);
+}
